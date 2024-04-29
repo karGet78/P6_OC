c6c22df7a73a0403333c13c4b847b9105f74c988
Karbou Ahmed <3karbou@gmail.com>
2024-04-29 10:14:01 +0200

    Ajout lien Admin

--- Planty/wp-content/themes/Planty/functions.php
+++ Planty/wp-content/themes/Planty/functions.php
@@ -9,3 +9,18 @@ function theme_enqueue_styles(){
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 
 }
+
+function add_admin_link_to_nav_menu($menu_nav) {
+    // Vérifier si l'utilisateur est connecté
+    if (is_user_logged_in()) {
+        // Créer le lien "Admin"
+        $admin_link = '<li><a href="' . admin_url() . '">Admin</a></li>';
+        
+        // Remplacer toutes les occurrences de "Nous rencontrer" par "Nous rencontrer" suivi du lien "Admin"
+        $menu_nav = str_replace('Nous rencontrer', 'Nous rencontrer' . $admin_link, $menu_nav);
+    }
+    
+    return $menu_nav;
+}
+add_filter('wp_nav_menu_items', 'add_admin_link_to_nav_menu');
+
